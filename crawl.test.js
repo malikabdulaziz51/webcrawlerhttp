dafa32623b326928e82fcb88bff32e68623151a3
malik abdul aziz <malikabdulaziz51@gmail.com>
2023-07-19 00:17:00 +0700

    getURLsFromHTML function and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocol", () => {
@@ -26,5 +26,72 @@ test("normalizeURL http protocol", () => {
   const input = "http://BLOG.boot.dev/path/";
   const output = normalizeURL(input);
   const expected = "blog.boot.dev/path";
+  expect(output).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputHTMLBody = `
+<html>
+    <body>
+      <a href="https://google.com/path/">
+        Google
+      </a>
+    </body>
+</html>
+`;
+  const inputBaseURL = "https://google.com/path/";
+  const output = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://google.com/path/"];
+  expect(output).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHTMLBody = `
+<html>
+    <body>
+      <a href="/path/">
+        Google
+      </a>
+    </body>
+</html>
+`;
+  const inputBaseURL = "https://google.com";
+  const output = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://google.com/path/"];
+  expect(output).toEqual(expected);
+});
+
+test("getURLsFromHTML aboslute and relative", () => {
+  const inputHTMLBody = `
+<html>
+    <body>
+      <a href="https://google.com/path1/">
+        Google 1
+      </a>
+      <a href="/path2/">
+        Google 2
+      </a>
+    </body>
+</html>
+`;
+  const inputBaseURL = "https://google.com";
+  const output = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://google.com/path1/", "https://google.com/path2/"];
+  expect(output).toEqual(expected);
+});
+
+test("getURLsFromHTML bad URL", () => {
+  const inputHTMLBody = `
+<html>
+    <body>
+      <a href="invalid">
+        Invalid URL
+      </a>
+    </body>
+</html>
+`;
+  const inputBaseURL = "https://google.com";
+  const output = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
   expect(output).toEqual(expected);
 });
